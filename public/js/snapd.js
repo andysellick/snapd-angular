257f1f6d70f6f12bfa17324b40a58f45754c854b
andysellick <andy.sellick@tangentsnowball.com>
2015-08-19 10:52:29 +0100

    Bug fixes, SEO urls on page reload, basic styling

--- public/js/snapd.js
+++ public/js/snapd.js
@@ -14,6 +14,27 @@ angular.module('snapd',[]).controller('snapdc',function($scope,$http,$window,$ti
     $scope.url_sitepath = '/snapd-angular'; //FIXME
     $scope.url_fullpath = $scope.url_base + $scope.url_sitepath;
 
+    //on page load figure out what the current url is and therefore what to show
+    $scope.getCurrentLocation = function(){
+        var here = window.location.href;
+        here = here.replace($scope.url_fullpath,'');
+        here = here.split('/');
+        here.clean('');
+        console.log(here);
+        if(here.length > 0){
+            if(here[0] == 'album'){
+                var url = '/album-data/' + here[1] + '/' + here[2] + '/' + here[3] + '/' + here[4] + '/'; //FIXME
+                $scope.currentpic = parseInt(here[5]);
+                $scope.getAlbum(url,here[0]);
+            }
+            else if(here[0] == 'thumbs'){
+            }
+        }
+        else {
+            $scope.getAlbumList(); //we're on the homepage, init
+        }
+    }
+
     //retrieve JSON list of all albums via ajax (on homepage load)
     $scope.getAlbumList = function(event){
         if($scope.albums.length){
@@ -34,15 +55,11 @@ angular.module('snapd',[]).controller('snapdc',function($scope,$http,$window,$ti
             });
         }
     }
-    $scope.getAlbumList();
 
     //called on page resize, checks size of page and sets dimensions for album pics accordingly
     $scope.getAlbumPicSize = function(){
-        var page = document.getElementsByTagName('html')[0];
-        $scope.albumpicheight = page.offsetHeight - 100;
-        //var im = document.getElementsByClassName('imagewrapper selected');
-        //$scope.albumpicwidth = im.offsetWidth;
-        $scope.albumpicwidth = page.offsetWidth;
+        $scope.albumpicwidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+        $scope.albumpicheight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
         console.log('getAlbumPicSize ',$scope.albumpicwidth,$scope.albumpicheight);
     }
 
@@ -63,7 +80,6 @@ angular.module('snapd',[]).controller('snapdc',function($scope,$http,$window,$ti
         else {
             $scope.currentview = view;
         }
-        $scope.getAlbumPicSize();
     }
 
     //show album
@@ -125,5 +141,18 @@ angular.module('snapd',[]).controller('snapdc',function($scope,$http,$window,$ti
             $scope.setView(e.state.view,e.state.pic,e.state.album);
         }
     };
+
+    $scope.getAlbumPicSize();
+    $scope.getCurrentLocation(); //on page load, check to see where we are
 });
 
+//http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {         
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
